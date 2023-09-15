de00bc7d5c1ecd5b39c116661576cf4ce893a223
byh <hy@.1023>
2023-09-15 18:12:38 +0800

    1：watermelon-cloud 工程结构优化 2：RegisteredClientRepository、UserDetailsService、UserDetails扩展

--- watermelon-authorization/watermelon-authorization-server/src/main/java/com/watermelon/authorization/WatermelonAuthorizationApplication.java
+++ watermelon-authorization/watermelon-authorization-server/src/main/java/com/watermelon/authorization/WatermelonAuthorizationApplication.java
@@ -2,6 +2,7 @@ package com.watermelon.authorization;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author byh
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @description
  */
 @SpringBootApplication
+@ComponentScan("com.watermelon.**")
 public class WatermelonAuthorizationApplication {
     public static void main(String[] args) {
         SpringApplication.run(WatermelonAuthorizationApplication.class, args);
